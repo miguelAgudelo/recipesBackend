2faca5ce26175e161a71e368ad97cfe580d96a61
LAPTOP-37NTF1A1\migue <miguelhxc37@gmail.com>
2019-11-28 23:07:33 -0500

    folder configs and upload image default

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+  
 'use strict'
 
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = 3900;
 
-mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/cocktail', { useNewUrlParser: true })
-        .then(() => {
-            console.log('Conexión a la base de datos correcta !!!');
-            // Crear servidor y ponerme a escuchar peticiones HTTP
-            app.listen(port, () => {
-                console.log('Servidor corriendo en http://localhost:'+port);
-            });
-
+    .then(() => {
+        console.log('Conexión a la base de datos correcta !!!');
+        // Crear servidor y ponerme a escuchar peticiones HTTP
+        app.listen(port, () => {
+            console.log('Servidor corriendo en http://localhost:'+port);
         });
+
+});
